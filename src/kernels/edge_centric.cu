3b08cd260cf2ffcf3248f3f563f64a10566c9bbc
ayush <ayushpareek1980@gmail.com>
2025-07-16 02:11:36 +0530

    removed class

--- src/kernels/edge_centric.cu
+++ src/kernels/edge_centric.cu
@@ -1,16 +1,26 @@
 #include "../inc/kernels/edge_centric.cuh"
 
-__global__ void Edge_centric_kernels::edge_centric_bfs(COO* graph,unsigned int* level,unsigned int* newVertexVisited, unsigned int currLevel){
-    unsigned int edge=blockIdx.x*blockDim.x + threadIdx.x;
-    if(edge< graph.numEdges){
-        unsigned int previous=graph.offsets[edge];
-        if(level[previous]==currLevel){
-            unsigned int neighbor=graph.indices[edge];
-            if(level[neighbor]==UINT_MAX){
-                level[neighbor]=currLevel;
-                *newVertexVisited=1;
+namespace craph {
+
+__global__ void edge_centric_bfs(
+    int* d_offsets,
+    int* d_indices,
+    int num_edges,
+    unsigned int* level,
+    unsigned int* newVertexVisited, 
+    unsigned int currLevel
+) {
+    unsigned int edge = blockIdx.x * blockDim.x + threadIdx.x;
+    if (edge < num_edges) {
+        unsigned int previous = d_offsets[edge];
+        if (level[previous] == currLevel) {
+            unsigned int neighbor = d_indices[edge];
+            if (level[neighbor] == UINT_MAX) {
+                level[neighbor] = currLevel;
+                *newVertexVisited = 1;
             }
         }
     }
-
 }
+
+} // namespace craph
--- src/kernels/vertex_centric.cu
+++ src/kernels/vertex_centric.cu
@@ -1,58 +1,62 @@
 #include "../inc/kernels/vertex_centric.cuh"
 
-__global__ void VertexCentric_kernel::top_down_bfs_kernel(
-    CSR *d_csr,
+namespace craph {
+
+__global__ void top_down_bfs_kernel(
+    int* d_offsets,
+    int* d_indices,
+    int num_vertices,
     unsigned int *level,
     unsigned int* vertex_visited,
     unsigned int current_level
 ) {
-    unsigned int vertex_id=blockIdx.x * blockDim.x + threadIdx.x;
-    if(vertex_id < d_csr.numVertices){
-        if(level[vertex_id]!=current_level-1){
-            for(int vertex=d_csr.g.offsets[vertex_id];vertex<d_csr.g.offsets[vertex_id+1];vertex++){
-                unsigned int next_Vertex=d_csr.g.indices[vertex];
-                if(level[next_Vertex]!=INT_MAX){
-                    level[next_Vertex]=current_level;
-                    *vertex_visited=1;
+    unsigned int vertex_id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (vertex_id < num_vertices) {
+        if (level[vertex_id] != current_level - 1) {
+            for (int vertex = d_offsets[vertex_id]; vertex < d_offsets[vertex_id + 1]; vertex++) {
+                unsigned int next_Vertex = d_indices[vertex];
+                if (level[next_Vertex] != INT_MAX) {
+                    level[next_Vertex] = current_level;
+                    *vertex_visited = 1;
                 }
             }
         }
     }
 }
 
-__global__ void VertexCentric_kernel::bottom_up_Frontier_kernel(
-    CSR* d_csr,
+__global__ void top_down_frontiers_kernel(
+    int* d_offsets,
+    int* d_indices,
     unsigned int *level,
     unsigned int current_level,
     unsigned int* prevFrontier,
     unsigned int* __lenprevFrontier,
     unsigned int* currFrontier,
     unsigned int* __lencurrFrontier
-){
-    __shared__ unsigned int currFrontier s[LOCAL_FRONTIER_CAPACITY];
+) {
+    __shared__ unsigned int currFrontier_s[LOCAL_FRONTIER_CAPACITY];
     __shared__ unsigned int numCurrFrontier_s;
     
-    if(threadIdx.x==0){
-        numCurrFrontier_s=0;
+    if (threadIdx.x == 0) {
+        numCurrFrontier_s = 0;
     }
 
     __syncthreads();
 
-    unsigned int idx=blockDim.x*blockIdx.x + threadIdx.x;
-    if(idx < __lenprevFrontier){
-        unsigned int vertex=prevFrontier[idx];
-        for(int edge=d_csr.offsets[vertex];edge<d_csr.offsets[vertex+1];edge++){
-            unsigned int dst=d_csr.indices[edge];
-            if(atomicCAS(&level[dst],UINT_MAX,current_level)==UINT_MAX){
-                unsigned int currFrontierIdx_s=atomicadd(&numCurrFrontier_s,1);
+    unsigned int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    if (idx < *__lenprevFrontier) {
+        unsigned int vertex = prevFrontier[idx];
+        for (int edge = d_offsets[vertex]; edge < d_offsets[vertex + 1]; edge++) {
+            unsigned int dst = d_indices[edge];
+            if (atomicCAS(&level[dst], UINT_MAX, current_level) == UINT_MAX) {
+                unsigned int currFrontierIdx_s = atomicAdd(&numCurrFrontier_s, 1);
                 
-                if(currFrontierIdx_s< LOCAL_FRONTIER_CAPACITY){
-                    currFrontier_s[currFrontierIdx_s]=dst;
-                }
-                else{
-                    numCurrFrontier_s=LOCAL_FRONTIER_CAPACITY;
-                    unsigned int IdxUpdate=atomicadd(&__lencurrFrontier,1);
-                    currFrontier[IdxUpdate]=dst;
+                if (currFrontierIdx_s < LOCAL_FRONTIER_CAPACITY) {
+                    currFrontier_s[currFrontierIdx_s] = dst;
+                } else {
+                    numCurrFrontier_s = LOCAL_FRONTIER_CAPACITY;
+                    unsigned int IdxUpdate = atomicAdd(__lencurrFrontier, 1);
+                    currFrontier[IdxUpdate] = dst;
                 }
             }
         }
@@ -60,48 +64,53 @@ __global__ void VertexCentric_kernel::bottom_up_Frontier_kernel(
 
     __syncthreads();
     unsigned int currStartIdx;
-    if(threadIdx.x==0){
-        currStartIdx= atomicadd(__lencurrFrontier,numCurrFrontier_s);
+    if (threadIdx.x == 0) {
+        currStartIdx = atomicAdd(__lencurrFrontier, numCurrFrontier_s);
     }
     __syncthreads();
-    for(unsigned int fillerIdx=threadIdx.x; fillerIdx < numCurrFrontier_S ; fillerIdx+=blockDim.x){
-        unsigned int fillerIdxTemp= currStartIdx + fillerIdx;
-        currFrontier[fillerIdxTemp]=currFrontier_s[fillerIdx];
+    for (unsigned int fillerIdx = threadIdx.x; fillerIdx < numCurrFrontier_s; fillerIdx += blockDim.x) {
+        unsigned int fillerIdxTemp = currStartIdx + fillerIdx;
+        currFrontier[fillerIdxTemp] = currFrontier_s[fillerIdx];
     }
-
 }
 
-__global__ void VertexCentric_kernel::bottom_up_bfs_kernel(
-    CSC *d_csc,
+__global__ void bottom_up_bfs_kernel(
+    int* d_offsets,
+    int* d_indices,
+    int num_vertices,
     unsigned int *level,
     unsigned int* vertex_visited,
     unsigned int current_level
-){
-   unsigned int vertex_id=blockIdx.x*blockDim.x + threadIdx.x;
-   if(vertex_id < d_csc.numVertices){
-        if(level[vertex_id]==INT_MAX){
-            for(int vertex=d_csc.g.offsets[vertex_id];vertex<d_csc.g.offsets[vertex_id];vertex++){
-                unsigned int next_vertex=d_csc.g.indices[vertex];
-                if(level[next_vertex]==current_level-1){
-                    level[vertex_id]=current_level;
-                    *vertex_visited=1;
+) {
+   unsigned int vertex_id = blockIdx.x * blockDim.x + threadIdx.x;
+   if (vertex_id < num_vertices) {
+        if (level[vertex_id] == INT_MAX) {
+            for (int vertex = d_offsets[vertex_id]; vertex < d_offsets[vertex_id + 1]; vertex++) {
+                unsigned int next_vertex = d_indices[vertex];
+                if (level[next_vertex] == current_level - 1) {
+                    level[vertex_id] = current_level;
+                    *vertex_visited = 1;
                     break;
                 }
             }
         }
    }
-
 }
 
-__global__ static void optimised_approach( 
-            CSC *d_csc,
-            CSR *d_csr,
-            unsigned int *level,
-            unsigned int* vertex_visited,
-            unsigned int current_level
-){
-    //implement soon
-    // intution is for the intial cases we uses top down and later bottom up
+__global__ void optimised_approach( 
+    int* d_csc_offsets,
+    int* d_csc_indices,
+    int* d_csr_offsets,
+    int* d_csr_indices,
+    int num_vertices,
+    unsigned int *level,
+    unsigned int* vertex_visited,
+    unsigned int current_level
+) {
+    // Implementation coming soon
+    // Intuition is for the initial cases we use top down and later bottom up
 }
+
+} // namespace craph
 
 
